a13619e62926dd237516f8620373bc67c7e78603
RMOATMAN <raemarie.oatman@gmail.com>
2021-08-01 12:39:30 -0400

    Completed all get routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,24 +3,39 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+//find all categories
+//be sure to include its associated Products
 router.get('/', async (req, res) => {
-  //find all categories
-  //be sure to include its associated Products
+
   try {
-    const CategoryData = await Category.findAll({
+    const categoryData = await Category.findAll({
       include:[{ model: Product }]
     });
-    res.status(200).json(CategoryData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
-  //console.log("Hello World");
+
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
+// find one category by its `id` value
+// be sure to include its associated Products
+  router.get('/:id', async (req, res) => {
+    try {
+      const categoryData = await Category.findByPk(req.params.id, {
+        include: [{ model: Product }]
+      });
+  
+      if (!categoryData) {
+        res.status(404).json({ message: 'No category found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 router.post('/', (req, res) => {
   // create a new category
