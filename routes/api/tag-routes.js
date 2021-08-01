c51a3dc4c7e8c9a9f099f3940ccb7d2532583804
RMOATMAN <raemarie.oatman@gmail.com>
2021-08-01 14:07:18 -0400

    Completed all Delete routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -44,8 +44,24 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+// delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+        },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
